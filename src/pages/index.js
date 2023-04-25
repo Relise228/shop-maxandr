54ba29813bccfbe9fbf2d9d239caeda44bd3d53d
Andrii Yuzvyk <andreysmit43@gmail.com>
2023-04-25 19:59:35 +0300

    flyoutmenu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import { Inter } from 'next/font/google';
-import Layout from '@components/Layout';
-import { useSession } from 'next-auth/react';
+// import { Inter } from 'next/font/google';
+import Layout from "@components/Layout"
+import { useSession } from "next-auth/react"
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  const { data: session } = useSession();
-  console.log(session, 'session');
+  const { data: session } = useSession()
+  console.log(session, "session")
   return (
     <Layout>
-      Hey {session ? JSON.stringify(session, null, 2) : 'stranger'}
+      Hey {session ? JSON.stringify(session, null, 2) : "stranger"}
     </Layout>
-  );
+  )
 }
