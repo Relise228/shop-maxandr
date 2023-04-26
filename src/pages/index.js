7728eaec8359bb2df9edf53e7cc6269d1720c395
Maksym <mlysenko@solvexus.com>
2023-04-26 17:50:59 +0300

    auth is improved (only googleAuth+admin page)

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   const { data: session } = useSession();
-  console.log(session, 'session');
   return (
     <Layout>
       Hey {session ? JSON.stringify(session, null, 2) : 'stranger'}
